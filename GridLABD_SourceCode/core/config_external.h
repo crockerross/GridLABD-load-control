35fb60d9af3546f59cbae2c059d982b26ed2426d
crockerross <41803745+crockerross@users.noreply.github.com>
2018-09-07 10:56:32 -0400

    source code
    files for modified gridlabd source code

---
+++ GridLABD_SourceCode/core/config_external.h
@@ -0,0 +1,28 @@
+// config.h automatically created Thu 06/23/2016 11:09:01.86 
+// configure: config=config_external.h 
+// configure: search=C:\ 
+// configure: source=.. 
+// : MYSQL target include\mysql.h not found  
+// 
+// MATLAB x64 configuration 
+// 
+// configure: found C:\PROGRA~1\MATLAB\R2014b\extern\include\mex.h 
+#if defined _M_X64 || defined _M_IA64 
+#define MATLAB_X64 
+#endif 
+ 
+#ifdef MATLAB_X64 
+#define MATLAB_INCLUDE "C:\PROGRA~1\MATLAB\R2014b\extern\include" 
+#define MATLAB_LIB "C:\PROGRA~1\MATLAB\R2014b\extern\lib\win64\MICROS~1" 
+#define HAVE_MATLAB "C:\PROGRA~1\MATLAB\R2014b" 
+#endif 
+// configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// configure: PowerWorld SimAuto target RegSimAuto.exe not found  
